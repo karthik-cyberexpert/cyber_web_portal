b5fb643ad944ebf240d407010afa9c5f0f05132a
karthik-cyberexpert <itzkarthik.cyber@gmail.com>
2026-01-29 09:45:03 +0530

    Added

--- timetable_migration.sql
+++ timetable_migration.sql
@@ -1,6 +1,8 @@
 -- Migration: Add Semester to Timetable Slots
 -- Description: Allows separate timetables for different semesters within the same batch/section history.
 
+SET SQL_SAFE_UPDATES = 0;
+
 -- 1. Add semester column
 ALTER TABLE timetable_slots ADD COLUMN semester INT DEFAULT NULL;
 
@@ -17,3 +19,5 @@ ALTER TABLE timetable_slots DROP INDEX unique_section_slot;
 
 -- Add new constraint including semester
 CREATE UNIQUE INDEX unique_section_slot_sem ON timetable_slots (section_id, day_of_week, period_number, semester);
+
+SET SQL_SAFE_UPDATES = 1;
